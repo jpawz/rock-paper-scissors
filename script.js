9c811a8b74716f8bd2f6e917342caa59ff5835b8
Paweł J <jpawz@outlook.com>
2023-01-07 18:11:54 +0100

    add getPlayerChoice function

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ["Rock", "Paper", "Scissors"];
 
-    const randomIndex = Math.floor(Math.random() * 3);
-    
-    return choices[randomIndex];
+  const randomIndex = Math.floor(Math.random() * 3);
+
+  return choices[randomIndex];
+}
+
+function getPlayerChoice() {
+  let answer;
+  do {
+    answer = prompt("Type Rock, Paper or Scissors");
+  } while (
+    answer.toLowerCase() != "rock" &&
+    answer.toLowerCase() != "paper" &&
+    answer.toLowerCase() != "scissors"
+  );
+
+  return answer[0].toUpperCase() + answer.substring(1).toLowerCase();
 }
