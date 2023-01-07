a88cff56f9740ad00457ba04b3939c9023a5ea97
Paweł J <jpawz@outlook.com>
2023-01-07 18:29:56 +0100

    add playRound function

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ function getPlayerChoice() {
 
   return answer[0].toUpperCase() + answer.substring(1).toLowerCase();
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "Match!";
+  }
+  if (playerSelection == "Rock") {
+    if (computerSelection == "Paper") {
+      return "You Lose! Paper beats Rock";
+    } else {
+      return "You Win! Rock beats Scissors";
+    }
+  }
+  if (playerSelection == "Paper") {
+    if (computerSelection == "Rock") {
+      return "You Win! Paper beats Rock";
+    } else {
+      return "You Lose! Rock beats Scissors";
+    }
+  }
+  if (playerSelection == "Scissors") {
+    if (computerSelection == "Paper") {
+      return "You Win! Scissors beats Paper";
+    } else {
+      return "You Lose! Rock beats Scissors";
+    }
+  }
+}
